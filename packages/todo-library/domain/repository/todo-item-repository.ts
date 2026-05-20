0070ef9de605c90d6cecee34c85acbd921633121
leonardo terceiro (leiman) <suporte4@lefisc.com>
2026-05-20 16:43:35 -0300

    removido pacotes desnecessarios

--- packages/todo-library/domain/repository/todo-item-repository.ts
+++
@@ -1,9 +0,0 @@
-import { TodoItem } from "../entities/todo.entity";
-
-
-export interface TodoItemRepository {
-    save(todoItem: TodoItem): Promise<void>;
-    findById(id: string): Promise<TodoItem | null>;
-    findAll(): Promise<TodoItem[]>;
-    deleteById(id: string): Promise<void>;
-}
